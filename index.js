19bb44a17138fc708dd97c00ac20b2694b544c4e
lucasbrafer <lucasbrafer@gmail.com>
2018-12-03 21:52:43 -0200

    aplicacoes

---
+++ index.js
@@ -0,0 +1,35 @@
+const pkg = require('./package.json');
+const server = require('./lib/server');
+
+process.title = pkg.name;
+
+const shutdown = async () => {
+  console.info('Desligamento Gracioso em Andamento');
+  await server.stop();
+  process.exit(0);
+};
+
+process.on('SIGTERM', shutdown)
+  .on('SIGINT', shutdown)
+  .on('SIGHUP', shutdown)
+  .on('uncaughtException', err => {
+    console.error('uncaughtException caught the error: ', err);
+    throw err;
+  })
+  .on('unhandledRejection', (err, promise) => {
+    console.error(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
+    throw err;
+  })
+  .on('exit', (code) => {
+    console.info(`Node process exit with code: ${code}`);
+  });
+
+  (async () => {
+    try {
+      await server.start();
+    } catch (err) {
+      console.error('[APP] initialization failed', err);
+      throw err;
+    }
+    console.info('[APP] initialized SUCCESSFULLY');
+  })();
